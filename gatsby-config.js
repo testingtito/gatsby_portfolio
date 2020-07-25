988b2962e8e9d475679973a8c5fa6e34e0e09d09
Toby Chee <testingtito@gmail.com>
2020-07-25 00:40:36 -0700

    project is ready to be deployed

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,19 +35,19 @@ module.exports = {
         //  ONLY ADD TO ARRAY IF YOU HAVE DATA IN STRAPI !!!!
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-prefetch-google-fonts`,
-    //   options: {
-    //     fonts: [
-    //       {
-    //         family: `Roboto`,
-    //         variants: [`400`, `700`],
-    //       },
-    //       {
-    //         family: `Open Sans`,
-    //       },
-    //     ],
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Roboto`,
+            variants: [`400`, `700`],
+          },
+          {
+            family: `Open Sans`,
+          },
+        ],
+      },
+    },
   ],
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,6 @@ import Image from "gatsby-image"
 import { Link } from "gatsby"
 import { graphql, useStaticQuery } from "gatsby"
 import SocialLinks from "../constants/socialLinks"
-import socialLinks from "../constants/socialLinks"
 
 const query = graphql`
 {
